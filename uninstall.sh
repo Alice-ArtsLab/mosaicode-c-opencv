5669ab1adf2f4fe6e4f9a1985dc506fec39ee8a5
llgoncalves <luanlg.cco@gmail.com>
2017-08-09 12:41:07 -0300

    Fixing uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@
 python setup.py install --record files.txt
 cat files.txt | xargs rm -rf
 rm -rf files.txt
-rm -rf /usr/share/mosaicode
 rm -rf build
 rm -rf dist
-rm -rf ~/mosaicode/configuration.xml
+rm -rf mosaicode_c_opencv.extensions.egg-info
+rm -rf usr/local/lib/python2.7/dist-packages/mosaicode_c_opencv.extensions-1.0-py2.7.egg
