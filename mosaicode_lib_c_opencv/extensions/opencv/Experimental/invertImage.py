754599529a5c8216837d71274841154dc6202883
fred <fredribeiro97@gmail.com>
2018-05-17 16:52:34 -0300

    Invert Image block add

---
+++ mosaicode_lib_c_opencv/extensions/opencv/Experimental/invertImage.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This module contains the Sobel class.
+"""
+
+from mosaicode.GUI.fieldtypes import *
+from mosaicode.model.blockmodel import BlockModel
+
+class FaceDetection(BlockModel):
+	"""
+	This class contains methods related the FaceDetection class.
+	"""
+
+	def __init__(self):
+		BlockModel.__init__(self)
+
+		self.language = "c"
+		self.framework = "opencv"
+		self.label = "Invert Image"
+		self.color = "90:5:10:150"
+		self.group = "Experimental"
+		self.ports = [{"type": "mosaicode_lib_c_opencv.extensions.ports.image",
+						"name": "input_image",
+						"label": "Input Match Object Image",
+						"conn_type":"Input"},
+						{"type": "mosaicode_lib_c_opencv.extensions.ports.image",
+						"name": "output_image",
+						"label": "Output Image",
+						"conn_type":"Output"}
+		]
+
+# --------------------------- C/OpenCV Code --------------------------- #
+
+		self.codes["declaration"] = \
+"""
+	Mat $port[input_image]$;
+	Mat $port[output_image]$;
+"""		
+
+		self.codes["execution"] = \
+"""
+	if(!$port[input_image]$.empty()){
+		flip($port[input_image]$, $port[output_image]$, 1);
+	}
+"""		
+		self.codes["deallocation"] = \
+"""
+	$port[input_image]$.release();
+	$port[output_image]$.release();
+"""		
+
+# --------------------------------------------------------------------- #
